885e60e2f99cacfc49d5097bbbf5ed0fe18f0920
Marcello Testi <marcello.testi@sparkfabrik.com>
2021-05-21 00:14:45 +0200

    fixing command

--- do-dup.sh
+++ do-dup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export SRC_HOST=${SRC_HOST}
 export SRC_NAME=${SRC_NAME}
 export SRC_USER=${SRC_USER}
@@ -13,14 +15,14 @@ export DST_PORT=${DST_PORT:-"3306"}
 
 DUMP_OPTIONS="--single-transaction --quick --skip-lock-tables --max-allowed-packet=1000000000"
 
-# Drop and recreate destination DB
-mysql -u${DST_USER} -p${DST_PASS} -h${DST_HOST} -P{$DST_PORT} --execute "DROP DATABASE IF EXISTS ${DST_NAME}"
-mysql -u${DST_USER} -p${DST_PASS} -h${DST_HOST} -P${DST_PORT} --execute "CREATE DATABASE ${DST_NAME}"
+echo "Dropping and recreating destination DB"
+mysql -u$DST_USER -p$DST_PASS -h$DST_HOST -P$DST_PORT --execute "DROP DATABASE IF EXISTS $DST_NAME"
+mysql -u$DST_USER -p$DST_PASS -h$DST_HOST -P$DST_PORT --execute "CREATE DATABASE $DST_NAME"
 
-# Dump source DB
-mysqldump -u${SRC_USER} -p${SRC_PASS} -h${SRC_HOST} -P${SRC_PORT} $DUMP_OPTIONS ${SRC_NAME} > dump.sql
+echo "Dumping the source DB"
+mysqldump -u$SRC_USER -p$SRC_PASS -h$SRC_HOST -P$SRC_PORT $DUMP_OPTIONS $SRC_NAME > dump.sql
 
-# Import into destination DB
-mysql -u${DST_USER} -p${DST_PASS} -h${DST_HOST} -P${DST_PORT} < dump.sql
+echo "Importing into the destination DB"
+mysql -u$DST_USER -p$DST_PASS -h$DST_HOST -P$DST_PORT $DST_NAME < dump.sql
 
 echo "DB Dup Operation Complete!"
